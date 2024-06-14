329b673614cf86be8406b938cbd2eea746c9e66f
yesanggl <yesanggl1@gmail.com>
2024-06-14 21:06:15 +0800

    first commit

---
+++ src/main/java/cn/ibaochenyu/bcyrailwayticketingsystem/BcyRailwayTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package cn.ibaochenyu.bcyrailwayticketingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcyRailwayTicketingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BcyRailwayTicketingSystemApplication.class, args);
+    }
+
+}
